83f185be240cc65a1c8a4e5a4022cc1ab4dd4c47
a7med3365 <a7med3365@gmail.com>
2023-12-30 18:22:03 +0300

    added project publish route

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -9,6 +9,7 @@ import {
 import { createProjectCtrl } from '../controllers/project/create';
 import { updateProjectCtrl } from '../controllers/project/update';
 import { currentUser } from '../middleware/logged-in-check';
+import { publishProjectCtrl } from '../controllers/project/publish';
 
 const router = express.Router();
 
@@ -47,16 +48,31 @@ router.post(
 router.put(
   '/api/projects/:projectId',
   [
+    param('projectId').notEmpty().withMessage('Project ID is required'),
     body('title')
       .optional()
       .notEmpty()
       .withMessage('Project Title is required')
       .isLength({ min: 2, max: 30 })
       .withMessage('Title should be between 2 and 30 characters'),
+    body('progress')
+      .isEmpty()
+      .withMessage('Progress cannot be updated directly'),
   ],
   validateRequest,
   currentUser,
   updateProjectCtrl
+);
+
+router.put(
+  '/api/projects/:projectId/publish',
+  [
+    param('projectId').notEmpty().withMessage('Project ID is required'),
+    body('isPublished').notEmpty().withMessage('Publish status is required'),
+  ],
+  validateRequest,
+  currentUser,
+  publishProjectCtrl
 );
 
 export { router as projectRouter };
